a412220010bec4b09fb7b2f3099f5c95a51d87e6
the-first-elder <emmanuelmayowa95@gmail.com>
2023-07-15 10:53:17 +0100

    completed crud operations on Http server

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 const http = require("node:http");
 
 const fs = require("node:fs");
+const { title } = require("node:process");
 
 const DB = fs.readFileSync("./db/index.json", "utf-8");
 
@@ -49,6 +50,88 @@ const server = http.createServer((req, res) => {
     res.write(JSON.stringify(response));
     console.log(book);
     res.end();
+  }
+
+
+  //   UPDATE http://localhost:5000/books/id
+
+  if (url && method === "PATCH") {
+    const params = url.split("/");
+    const paramId = params[params.length - 1].trim();
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const books = JSON.parse(DB);
+    let data = ''
+    req.on('data', files => {
+      data += files
+
+    })
+
+    req.on('end', () => {
+      const book = books.map((el) => {
+        if (el.id === +paramId) {
+          data = JSON.parse(data)
+          el.title = data.title
+          el.pages = data.pages
+          el.publishedYear = data.publishedYear
+          el.price = data.price
+          el.quantity = data.quantity
+
+          console.log(books)
+          const changeData = fs.writeFileSync('./db/index.json', JSON.stringify(books, null, 2))
+          // data = files.toString()
+
+        }
+      });
+      // res.write
+      const response = { status: 200, message: 'Uploaded', data: { data } }
+      res.write(JSON.stringify(response))
+      res.end()
+    })
+
+
+
+
+
+
+
+    // if (book === undefined) {
+    //   res.writeHead(404, { "Content-Type": "text/plain" });
+    //   res.end("Book Not Found");
+    // }
+
+    // const response = { status: 200, message: "OK", data: { book } };
+    // res.write(JSON.stringify(response));
+    // console.log(book);
+    // res.end();
+  }
+  //  DELETE http://localhost:5000/books/id
+
+  if (url && method === "DELETE") {
+    console.log('hi')
+    const params = url.split("/");
+    const paramId = params[params.length - 1].trim();
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const books = JSON.parse(DB);
+    let data = ''
+
+    req.on('data', files => {
+      data += files.toString()
+
+    })
+
+    req.on('end', () => {
+
+      const book = books.filter((el) => el.id !== +paramId);
+
+      const changeData = fs.writeFileSync('./db/index.json', JSON.stringify(book, null, 2))
+      data = JSON.parse(data)
+      // res.write
+      const response = { status: 200, message: 'Data succesfully deleted', data: { data } }
+      res.write(JSON.stringify(response))
+      res.end()
+    })
   }
 });
 
